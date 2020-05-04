91f76245429508d2ab29cf116a0592548cabbb66
kakapo97 <34475753+kakapo97@users.noreply.github.com>
2020-05-04 18:19:26 +0200

    lekcja3

---
+++ lekcja3/src/views/searchMovie/SearchMovie.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavPanel from '../../components/navPanel/NavPanel'
+
+const SearchMovie = () => {
+
+    return (
+        <div>
+            <NavPanel />
+
+        </div>
+    );
+};
+
+export default SearchMovie;
